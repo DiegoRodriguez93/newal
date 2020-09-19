0be86725b01f85d35636ca49d783ef186f04e472
elDiego <d.rodriguez@vida.com.uy>
2020-09-19 16:41:49 -0300

    123

--- js/header.js
+++ js/header.js
@@ -7,8 +7,8 @@ var header = `<div class="container-fluid">
         </div>
         <nav class="header__menu mobile-menu">
             <ul>
-                <li class="active"><a href="./index.html">Inicio</a></li>
-                <li><a href="#">Cursos</a>
+                <li><a href="./index.html">Inicio</a></li>
+                <li class="active"><a href="#">Cursos</a>
                     <ul class="dropdown">
                         <li><a href="./about.html">Aperturas</a></li>
                         <li><a href="./episodes-details.html">Medio juego</a></li>
